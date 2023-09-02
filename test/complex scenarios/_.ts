0072c56130c4d0ebe609d1e6699f749a30a103d7
0xslava <slava@zoodao.com>
2023-09-02 11:22:01 +0300

    Wrote test to check that repeating bridge txs become cheaper

--- test/complex scenarios/_.ts
+++ test/complex scenarios/_.ts
@@ -1,7 +1,9 @@
 import { bridgeWithChangingOwner } from './bridgeWithChangingOwner'
+import { cheaperRepeatedBridges } from './cheaperRepetingBridges'
 import { complexRoute } from './complexRoute'
 
 describe('Special test scenarios', function () {
 	it('complex route', complexRoute)
 	it('bridge with changing owner', bridgeWithChangingOwner)
+	it('after deploy of copy bridges should be cheaper', cheaperRepeatedBridges)
 })
---
+++ test/complex scenarios/cheaperRepetingBridges.ts
@@ -0,0 +1,72 @@
+import { expect } from 'chai'
+import { deployMultipleBridges } from './_.fixtures'
+import { deployNFTWithMint, getAdapterParamsAndFeesAmount } from '../Bridge/_.fixtures'
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
+import hardhat, { ethers } from 'hardhat'
+import { NFT } from '../../typechain-types'
+
+export const cheaperRepeatedBridges = async function () {
+	hardhat.tracer.enabled = false
+	const { nft, owner, tokenId, signers } = await loadFixture(deployNFTWithMint)
+	const { ethBridge, moonBridge, networkIds, lzEndpoints } = await loadFixture(deployMultipleBridges)
+
+	await nft.approve(ethBridge.address, tokenId)
+
+	const { fees, adapterParams } = await getAdapterParamsAndFeesAmount(
+		nft,
+		tokenId,
+		owner,
+		networkIds.moonNetworkId,
+		ethBridge,
+		lzEndpoints.ethLzEndpoint
+	)
+
+	const zeroAddress = ethers.constants.AddressZero
+
+	expect(await nft.ownerOf(tokenId)).to.be.eq(owner.address)
+
+	const tx = await ethBridge.bridge(
+		nft.address,
+		tokenId,
+		networkIds.moonNetworkId,
+		owner.address,
+		zeroAddress,
+		adapterParams,
+		{
+			value: fees[0],
+		}
+	)
+	0.022006028
+	expect(tx).not.to.be.reverted
+
+	await nft.mint(signers[6].address, 1)
+	await nft.connect(signers[6]).approve(ethBridge.address, tokenId + 1)
+
+	hardhat.tracer.enabled = true
+
+	const gas = '350000'
+	const newAdapterParams = ethers.utils.solidityPack(['uint16', 'uint256'], [1, gas])
+	const abi = new ethers.utils.AbiCoder()
+
+	const payload = abi.encode(
+		['address', 'string', 'string', 'uint256', 'string', 'address'],
+		[nft.address, await nft.name(), await nft.symbol(), tokenId, await nft.tokenURI(tokenId), owner.address]
+	)
+
+	const lesserFees = await lzEndpoints.ethLzEndpoint.estimateFees(
+		networkIds.moonNetworkId,
+		ethBridge.address,
+		payload,
+		false,
+		newAdapterParams
+	)
+
+	const repeatedBridge = await ethBridge
+		.connect(signers[6])
+		.bridge(nft.address, tokenId + 1, networkIds.moonNetworkId, owner.address, zeroAddress, newAdapterParams, {
+			value: lesserFees[0],
+		})
+
+	expect(repeatedBridge).to.emit(moonBridge, 'NFTBridged')
+	// expect(repeatedBridge).not.to.be.reverted
+}
