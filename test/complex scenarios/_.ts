e7931cccbbf8c969dde66f1fe3ede2eaeb6adda0
0xslava <slava@zoodao.com>
2023-09-02 11:22:01 +0300

    Added one more complex test scenario "meet in the middle"

--- test/complex scenarios/_.ts
+++ test/complex scenarios/_.ts
@@ -1,9 +1,14 @@
 import { bridgeWithChangingOwner } from './bridgeWithChangingOwner'
 import { cheaperRepeatedBridges } from './cheaperRepetingBridges'
 import { complexRoute } from './complexRoute'
+import { meetInTheMiddle } from './meetInTheMiddle'
 
 describe('Special test scenarios', function () {
 	it('complex route', complexRoute)
 	it('bridge with changing owner', bridgeWithChangingOwner)
 	it('after deploy of reflection bridges should be cheaper', cheaperRepeatedBridges)
+	it.only(
+		'simultaneous bridge from one network to two others and sending reflection to another reflection contract should make ReflectedNFT contract with 2 NFTs on it',
+		meetInTheMiddle
+	)
 })
---
+++ test/complex scenarios/meetInTheMiddle.ts
@@ -0,0 +1,78 @@
+import { expect } from 'chai'
+import { deployMultipleBridges } from './_.fixtures'
+import { deployNFTWithMint, getAdapterParamsAndFeesAmount } from '../Mirror/_.fixtures'
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
+import { ethers } from 'hardhat'
+import { expectToBeRevertedWith } from '../_utils'
+
+export const meetInTheMiddle = async function () {
+	const { nft, owner, tokenId } = await loadFixture(deployNFTWithMint)
+	const { ethBridge, moonBridge, arbBridge, networkIds, lzEndpoints } = await loadFixture(deployMultipleBridges)
+	await ethBridge.changeCollectionEligibility(nft.address, true)
+
+	await nft.mint(owner.address, 1)
+	await nft.setApprovalForAll(ethBridge.address, true)
+
+	const { fees, adapterParams } = await getAdapterParamsAndFeesAmount(
+		nft,
+		[tokenId],
+		networkIds.moonNetworkId,
+		ethBridge,
+		false
+	)
+
+	const zeroAddress = ethers.constants.AddressZero
+
+	await ethBridge.createReflection(
+		nft.address,
+		[tokenId],
+		networkIds.moonNetworkId,
+		owner.address,
+		zeroAddress,
+		adapterParams,
+		{
+			value: fees[0],
+		}
+	)
+
+	await ethBridge.createReflection(
+		nft.address,
+		[tokenId + 1],
+		networkIds.arbNetworkId,
+		owner.address,
+		zeroAddress,
+		adapterParams,
+		{
+			value: fees[0],
+		}
+	)
+
+	expect(await nft.ownerOf(tokenId)).to.be.eq(ethBridge.address)
+	expect(await nft.ownerOf(tokenId + 1)).to.be.eq(ethBridge.address)
+
+	const arbReflectionAddr = await arbBridge.reflection(nft.address)
+
+	const moonReflectionNftAddr = await moonBridge.reflection(nft.address)
+	const moonReflection = await ethers.getContractAt('ReflectedNFT', moonReflectionNftAddr)
+
+	await moonBridge.createReflection(
+		moonReflectionNftAddr,
+		[tokenId],
+		networkIds.arbNetworkId,
+		owner.address,
+		zeroAddress,
+		adapterParams,
+		{
+			value: fees[0],
+		}
+	)
+
+	await expectToBeRevertedWith(moonReflection.ownerOf(tokenId), 'ERC721: invalid token ID')
+
+	// Checkpoint on arbitrum contract
+	const arbReflection = await ethers.getContractAt('ReflectedNFT', arbReflectionAddr)
+	expect(await arbReflection.balanceOf(owner.address)).to.be.eq(2)
+
+	expect(await arbReflection.ownerOf(tokenId)).to.be.eq(owner.address)
+	expect(await arbReflection.ownerOf(tokenId + 1)).to.be.eq(owner.address)
+}
