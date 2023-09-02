50684140a26ea965caa5bf9ca9a6ba0fb08bb2d5
0xslava <slava@zoodao.com>
2023-09-02 11:22:01 +0300

    Added ethereum bridge address

--- constants/bridgeAddresses.ts
+++ constants/bridgeAddresses.ts
@@ -6,7 +6,7 @@ export const bridgeAddresses = {
 		arbitrum: '',
 	},
 	production: {
-		ethereum: '',
+		ethereum: '0x6668516859dD2e94Fe630a0370ae9986FA1902d6',
 		moonbeam: '0x36a65778e80Aa9E0BFe4458a049536FE66cec8a0',
 		arbitrum: '0xB96050683438028FF4057fd99270EcA95e9103Ed',
 		fantom: '',
