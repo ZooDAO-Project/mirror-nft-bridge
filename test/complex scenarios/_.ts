96933f70932cb90e853897c0921de73f9145f78d
0xslava <slava@zoodao.com>
2023-09-02 11:22:01 +0300

    Deleted .only

--- test/complex scenarios/_.ts
+++ test/complex scenarios/_.ts
@@ -7,7 +7,7 @@ describe('Special test scenarios', function () {
 	it('complex route', complexRoute)
 	it('bridge with changing owner', bridgeWithChangingOwner)
 	it('after deploy of reflection bridges should be cheaper', cheaperRepeatedBridges)
-	it.only(
+	it(
 		'simultaneous bridge from one network to two others and sending reflection to another reflection contract should make ReflectedNFT contract with 2 NFTs on it',
 		meetInTheMiddle
 	)
