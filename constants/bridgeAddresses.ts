812470255c52881ff8d08086890cc8b8b85600b6
0xslava <slava@zoodao.com>
2023-09-02 11:22:01 +0300

    Added file with deployed bridge addresses

---
+++ constants/bridgeAddresses.ts
@@ -0,0 +1,14 @@
+export const bridgeAddresses = {
+	test: {
+		ethereum: '',
+		fantom: '0x7cd3e6e1A69409deF0D78D17a492e8e143F40eC5',
+		moonbeam: '0xeE2ebe1101B0243A1cb395687149c1627eE7B45e',
+		arbitrum: '',
+	},
+	production: {
+		ethereum: '',
+		moonbeam: '',
+		arbitrum: '',
+		fantom: '',
+	},
+}
